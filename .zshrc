86f61517de734ddd7385f1b4e6e1cc536344877e
bazuin-32 <ameenpiano@gmail.com>
2022-03-17 17:15:08 -0600

    Add xkcd function and display latest comic in zshrc

--- .zshrc
+++ .zshrc
@@ -1,4 +1,6 @@
+source ~/.config/xkcd.zsh
 neofetch
+xkcd newest
 
 # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
 # Initialization code that may require console input (password prompts, [y/n]
---
+++ xkcd.zsh
@@ -0,0 +1,36 @@
+function xkcd () {
+    comicNum=$1
+    newestJson=$(curl -s https://xkcd.com/info.0.json)
+    newestNum=$(jq -r .num <<< $newestJson)
+
+    if [[ "$1" != "newest" ]] ; then
+        re='^[0-9]+$'
+        # Check if argument is not "new", not a number or otherwise not valid and select random comic
+        if ! [[ $1 =~ $re ]] || [[ comicNum -lt 1 ]] || [[ "$comicNum" -gt "$newestNum" ]] ; then
+            # Generate a random comic number if none is specified
+            comicNum=$((1 + $RANDOM % $newestNum))
+        fi
+
+        # Get xkcd by number
+        json=$(curl -s https://xkcd.com/$comicNum/info.0.json)
+    else
+        # Use newest
+        comicNum=$newestNum
+        json=$newestJson
+    fi
+    
+
+    # Parse response using jq
+    url=$(jq -r .img <<< $json)
+    title=$(jq -r .title <<< $json)
+    alt=$(jq -r .alt <<< $json)
+    transcript=$(jq -r .transcript <<< $json)
+    date="$(jq -r .day <<< $json).$(jq -r .month <<< $json).$(jq -r .year <<< $json)"
+    
+    echo $url
+    echo "\n\"$title\"\n"
+    echo "#$comicNum -- $date\n"
+    
+    # Display the image using convert. Requires alacritty-sixel-git instead of alacritty
+    convert $url sixel:-
+}
