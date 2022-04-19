9ac38daa0d38906c825e4f8c9f03e14bcf438404
bazuin-32 <ameenpiano@gmail.com>
2022-04-18 19:34:11 -0600

    Make dim_scr function work for any set of monitors and temporarily disable it altogether

--- dim_scr.zsh
+++ dim_scr.zsh
@@ -1,4 +1,12 @@
 # gradually dims the screen
 dim_scr() {
-        for i in $(seq 1 -0.01 0.01); do xrandr --output HDMI-0 --brightness $i && xrandr --output HDMI-1 --brightness $i; done
+	local monitors=$(xrandr --listmonitors | grep -v 'Monitors:' | rev | cut -d ' ' -f 1 | rev | tr '\n' ' ')
+
+	return 0 # !! REMOVE !!
+
+        for i in $(seq 1 -0.01 0.01); do
+		for mon in $monitors; do
+			xrandr --output $mon --brightness $i
+		done
+	done
 }
