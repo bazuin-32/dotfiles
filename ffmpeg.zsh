48a201195f63d82a4557b5dec83b71479b9bf23c
bazuin-32 <ameenpiano@gmail.com>
2022-05-20 11:55:31 -0600

    Improvements to `mp42mov` function

--- ffmpeg.zsh
+++ ffmpeg.zsh
@@ -6,8 +6,6 @@
 # usage:
 # 	mp42mov input.mp4
 # 	mp42mov -r ~/Videos/some_folder
-#
-# TODO: allow multiple individual input files to be given at once
 function mp42mov {
 	local input_files=()
 
@@ -15,39 +13,51 @@ function mp42mov {
 		# recursive, next argument should be a directory
 		if [ ! -d "$2" ]; then
 			echo "$0: error: '$2' is not a directory" 1>&2
-			exit 1
+			return 1
 		fi
 
 		for file in "$2/**.{mp,MP}4"; do # some files use capitals (MP4), others use lowercase (mp4), so both are handled
 			input_files+=("$file")
 		done
 	elif [ -f "$1" ]; then
-		# we were given just a file, so just use it as long as it is an mp4
-		if [[ "$1" != *".mp4" ]]; then
-			echo "$0: error: '$1' is not an mp4 file" 1>&2
-			exit 1
-		fi
+		# we were given just a file, so we try to check all arguments
+		# in case more files were given, and make sure they are mp4
+		for arg in "$@"; do
+			extension="${$(basename -- $arg)##*.}"
+			
+			# TODO: fix this first if statement, its body runs even
+			# on valid mp4 files
+#			if [[ $extension != "mp4" || $extension != "MP4" ]]; then
+#				echo $extension
+#				echo "$0: warning: '$arg' is not an mp4 file, discarding and continuing..." 1>&2
+			if [ ! -f "$arg" ]; then
+				echo here
+				echo "$0: warning: '$arg' is not an mp4 file, discarding and continuing..." 1>&2
+			else
+				echo "adding $arg"
+				input_files+=("$arg")
+			fi
+		done
 
-		input_files+=("$1")
 	elif [[ "$1" != "" ]]; then
 		# we have some random input
 		echo "$0: error: '$1' is not a file" 1>&2
-		exit 1
+		return 1
 	else
 		# no argument was given
 		echo "$0: error: no input files" 1>&2
-		exit 1
+		return 1
 	fi
 
 	local num_files=${#input_files[@]}
 	if [ $num_files -lt 1 ]; then
 		echo "$0: error: no input files" 1>&2
-		exit 1
+		return 1
 	fi
 
 	for file in "${input_files[@]}"; do
 		file_no_ext="${file%.*}"
 
-		ffmpeg -hwaccel cuda -i "$file_no_ext.mp4" -vcodec dnxhd -acodec pcm_s24le -s 1920x1080 -r 60 -b:v 36M -pix_fmt yuv422p -f mov "$file_no_ext.mov"
+		ffmpeg -hwaccel cuda -i "$file" -vcodec dnxhd -acodec pcm_s24le -s 1920x1080 -r 60 -b:v 36M -pix_fmt yuv422p -f mov "$file_no_ext.mov"
 	done
 }
