a286de7c5c78943b05db3b6d332117f728481ab9
bazuin-32 <ameenpiano@gmail.com>
2022-05-21 17:13:55 -0600

    Add basic `xplr` config

---
+++ xplr/init.lua
@@ -0,0 +1,10 @@
+version = "0.17.6"
+
+xplr.config.node_types.directory.meta.icon = ""
+xplr.config.node_types.file.meta.icon = ""
+xplr.config.node_types.symlink.meta.icon = "🔗"
+
+xplr.config.node_types.special[".gitignore"] = {
+	meta = { icon = "" },
+	style = { fg = "Red" }
+}
