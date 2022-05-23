050c45cf0f27e1bc7d725d4b372b9e679f753a84
bazuin-32 <ameenpiano@gmail.com>
2022-05-23 16:13:59 -0600

    Add more extension-specific styling for xplr

--- xplr/init.lua
+++ xplr/init.lua
@@ -35,9 +35,16 @@ xplr.config.node_types.extension.pdf = {
 	meta = { icon = "" },
 	style = { fg = { Rgb = { 115, 115, 215 } } }
 }
+xplr.config.node_types.extension.xlsx = xplr.config.node_types.extension.pdf
 xplr.config.node_types.extension.json = {
 	meta = { icon = "" }
 }
+xplr.config.node_types.extension.mp4 = {
+	meta = { icon = "" },
+	style = { fg = { Rgb = { 160, 100, 220 } } }
+}
+xplr.config.node_types.extension.mov = xplr.config.node_types.extension.mp4
+xplr.config.node_types.extension.MP4 = xplr.config.node_types.extension.mp4
 
 
 -- special files
