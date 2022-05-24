410bd3cd3577dd7064598b98ddbbdc6f71449f8b
bazuin-32 <ameenpiano@gmail.com>
2022-05-24 10:55:01 -0600

    Add custom display format for file modified time

--- xplr/general.lua
+++ xplr/general.lua
@@ -5,3 +5,13 @@ xplr.config.node_types.symlink.meta.icon = "🔗"
 
 -- colors
 xplr.config.node_types.directory.style.fg = "Yellow"
+
+
+-- custom modified time display
+xplr.fn.custom.get_modified_time = function(path)
+	return tostring(os.date('%a %m/%d/%Y %I:%M %p', path.last_modified / 1000000000))
+end
+xplr.config.general.table.row.cols[5] = {
+	format = "custom.get_modified_time",
+	style = {}
+}
