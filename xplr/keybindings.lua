579ebbe8fc8fda548865a0d9b9d24ce48d2b2f97
bazuin-32 <ameenpiano@gmail.com>
2022-05-24 11:03:45 -0600

    Add `-k` flag to find in enter keybind for xplr

--- xplr/keybindings.lua
+++ xplr/keybindings.lua
@@ -5,15 +5,14 @@ xplr.config.modes.builtin.default.key_bindings.on_key.enter = {
 		{
 		BashExec = [===[
 		if [ -f "$XPLR_FOCUS_PATH" ]; then
-			if file "$XPLR_FOCUS_PATH" | grep -q text; then
+			if file -k "$XPLR_FOCUS_PATH" | grep -q text; then
 				$EDITOR "$XPLR_FOCUS_PATH"
 			else
 				xdg-open "$XPLR_FOCUS_PATH" & &>/dev/null
 			fi
 		fi
 		]===]
-		},
-		"Enter"
+		}
 	}
 }
 xplr.config.modes.builtin.default.key_bindings.on_key.z = {
