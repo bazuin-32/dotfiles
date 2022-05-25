40fafbb6036c5cfce3c73b9a0c28fee9143feea7
bazuin-32 <ameenpiano@gmail.com>
2022-05-24 19:43:34 -0600

    Improve bookmark removal dialog and prevent adding duplicate bookmarks in xplr config

--- xplr/bookmarks.lua
+++ xplr/bookmarks.lua
@@ -5,7 +5,11 @@ xplr.config.modes.builtin.default.key_bindings.on_key.m = {
 		{
 			BashExecSilently = [===[
 				PTH="${XPLR_FOCUS_PATH:?}"
-				if echo "${PTH:?}" >> "${XPLR_BOOKMARKS_FILE}"; then
+				
+				cat "${XPLR_BOOKMARKS_FILE}" | grep -q "${PTH:?}"
+				if [ $? -eq 0 ]; then
+					echo "LogWarning: ${PTH:?} is already in bookmarks, skipping duplicate" >> "${XPLR_PIPE_MSG_IN:?}"
+				elif echo "${PTH:?}" >> "${XPLR_BOOKMARKS_FILE}"; then
 					echo "LogSuccess: ${PTH:?} added to bookmarks" >> "${XPLR_PIPE_MSG_IN:?}"
 				else
 					echo "LogError: Failed to bookmark ${PTH:?}" >> "${XPLR_PIPE_MSG_IN:?}"
@@ -65,7 +69,26 @@ xplr.config.modes.builtin.default.key_bindings.on_key.u = {
 			# if the focused item is not in bookmarks, show fzf selection dialog
 			# to get the bookmark to remove
 			if [ $? -ne 0 ]; then 
-				PTH=$(cat "${XPLR_BOOKMARKS_FILE}" | fzf --no-sort)
+				field='\(\S\+\s*\)'
+				esc=$(printf '\033')
+				N="${esc}[0m"
+				R="${esc}[31m"
+				G="${esc}[32m"
+				Y="${esc}[33m"
+				B="${esc}[34m"
+				pattern="s#^([0-9]*)(\s*)(.*)/(.*)#$R\1$N\2$B\3/$N$Y\4$N#g"
+
+				PTH=$(cat "${XPLR_BOOKMARKS_FILE}" \
+				| sed 's/ /!#_#!/g' \
+				| nl \
+				| column -t \
+				| sed 's/!#_#!/ /g' \
+				| sed -E "${pattern}" \
+				| fzf --ansi \
+					--height '80%' \
+					--preview="echo \"{}\" | sed 's#.*->  ##;s#[[:digit:]]  ##'| xargs exa -lbghm@ --icons --git --color=always" \
+					--preview-window="right:50%" \
+				| sed -E 's#.*->  ##;s#([0-9]*)(\s*)/#/#g')
 			fi
 			
 			if [ "$PTH" ]; then
