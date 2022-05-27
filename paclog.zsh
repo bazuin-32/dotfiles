39abd6fbc42516e5fb8c6e0b1e048573fe3c98f6
bazuin-32 <ameenpiano@gmail.com>
2022-05-27 09:08:00 -0600

    Fix extra color codes being piped to bat in `paclog` function

--- paclog.zsh
+++ paclog.zsh
@@ -1,6 +1,6 @@
 function paclog () {
 	if [ "$1" = "today" ]; then
-		grep "$(date '+%Y-%m-%d')" /var/log/pacman.log --color=always | bat --file-name=/var/log/pacman.log
+		grep "$(date '+%Y-%m-%d')" /var/log/pacman.log --color=never | bat --file-name=/var/log/pacman.log
 	else
 		bat /var/log/pacman.log
 	fi
