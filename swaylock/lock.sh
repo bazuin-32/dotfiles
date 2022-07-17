32e144416d1b7ca09b3201adafbb4a8dba05648b
bazuin-32 <ameenpiano@gmail.com>
2022-07-17 11:30:45 -0600

    Add basic swaylock script with multi-monitor support

---
+++ swaylock/lock.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# get the current desktop screen and blur it
+monitors="$(hyprctl monitors -j | jq -r '.[] | .name' | tr '\n' ' ')"
+
+screenshot_dir="$(mktemp -d)"
+
+swaylock_args=()
+
+for monitor in ${monitors}; do
+    screenshot_file="${screenshot_dir}/${monitor}.png"
+
+    grim -o "${monitor}" "${screenshot_file}"
+    convert -filter Gaussian -resize 20% -blur 10x2 -resize 500% "${screenshot_file}" "${screenshot_file}"
+
+    swaylock_args+=("--image=${monitor}:${screenshot_file}")
+done
+
+swaylock ${swaylock_args[@]}
+
+rm -r "${screenshot_dir}"
