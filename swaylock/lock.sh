22f3bb5d4a2df964d3ff3720de7a56a80c487266
bazuin-32 <ameenpiano@gmail.com>
2022-07-25 09:06:46 -0600

    Add styling to swaylock

--- swaylock/lock.sh
+++ swaylock/lock.sh
@@ -5,7 +5,44 @@ monitors="$(hyprctl monitors -j | jq -r '.[] | .name' | tr '\n' ' ')"
 
 screenshot_dir="$(mktemp -d)"
 
-swaylock_args=()
+swaylock_args=(
+    --clock
+    --indicator
+    --indicator-idle-visible
+    --indicator-caps-lock
+
+    --indicator-radius 90
+
+    --font MesloLGSNF
+
+    --inside-color 282828
+    --inside-clear-color 282828
+    --inside-caps-lock-color 282828
+    --inside-ver-color 282828
+    --inside-wrong-color 282828
+
+    --key-hl-color ebdbb2
+
+    --line-color 3c3c3c
+    --line-clear-color 3c3c3c
+    --line-caps-lock-color 3c3c3c
+    --line-ver-color 3c3c3c
+    --line-wrong-color 3c3c3c
+
+    --ring-color 282828
+    --ring-clear-color 282828
+    --ring-caps-lock-color c89038
+    --ring-ver-color 282828
+    --ring-wrong-color 282828
+
+    --separator-color 8b7b52
+
+    --text-color ebdbb2
+    --text-clear-color ebdbb2
+    --text-caps-lock-color ebdbb2
+    --text-ver-color ebdbb2
+    --text-wrong-color ebdbb2
+)
 
 for monitor in ${monitors}; do
     screenshot_file="${screenshot_dir}/${monitor}.png"
