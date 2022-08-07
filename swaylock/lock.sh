c1fb7424b6c1b5460083c207e38aa63945434b8a
bazuin-32 <ameenpiano@gmail.com>
2022-08-06 18:10:43 -0600

    Turn off monitor when locked manually

--- swaylock/lock.sh
+++ swaylock/lock.sh
@@ -53,6 +53,13 @@ for monitor in ${monitors}; do
     swaylock_args+=("--image=${monitor}:${screenshot_file}")
 done
 
+# not sure why i didnt just do `swayidle ... &`; this is
+# how i wrote it as i was going but I think it looks nice
+# so im just keeping it
+{
+    swayidle -w timeout 300 'hyprctl dispatch dpms off'
+} &
+
 swaylock ${swaylock_args[@]}
 
 rm -r "${screenshot_dir}"
