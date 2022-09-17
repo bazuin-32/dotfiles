b569d0e04495b3865fa5274d81d87f8f26722c19
bazuin-32 <ameenpiano@gmail.com>
2022-09-17 08:26:30 -0600

    Finally get window_title working for eww bar

---
+++ eww/scripts/window_title.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+#######################################################################################
+# stolen from
+# https://github.com/taylor85345/neon-hyprland-theme/blob/main/eww/scripts/appname,
+# but I've modified it some
+#######################################################################################
+
+workspaces() {
+    if [[ ${1:0:12} == "activewindow" ]]; then #set focused workspace
+        string=${1:14}
+        class="${string/,*/}"
+        export title=${string/,/, }
+        [[ $title == ", " ]] && unset title
+    fi
+}
+
+module() {
+    eww update show_window_title=false
+    sleep 0.005
+    echo "${title}" | awk -F ', ' '{ print $2 }'
+    eww update show_window_title=true
+}
+
+socat -u UNIX-CONNECT:/tmp/hypr/"$HYPRLAND_INSTANCE_SIGNATURE"/.socket2.sock - | while read -r event; do 
+    workspaces "$event"
+    module
+done
