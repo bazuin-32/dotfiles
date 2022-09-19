8da9b579bab94f8b12b44550f14ab50b7545fdef
bazuin-32 <ameenpiano@gmail.com>
2022-09-19 16:47:20 -0600

    Change battery detection from a one-time `eww update` to a `defpoll` in eww config with a long interval

--- eww/start.sh
+++ eww/start.sh
@@ -9,6 +9,3 @@ monitors=$(hyprctl monitors -j | jq '.[] | .id')
 for monitor in ${monitors}; do
     eww open bar${monitor}
 done
-
-# enable battery module if a battery is present
-[[ -e /sys/class/power_supply/BAT0 ]] && eww update show_battery=true
