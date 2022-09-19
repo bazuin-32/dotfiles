2fd30d1afd8d9b094dcfd58df9cfa1d8d662bfc4
bazuin-32 <ameenpiano@gmail.com>
2022-09-19 12:32:00 -0600

    Style active workspace in eww bar

---
+++ eww/scripts/active_workspace.sh
@@ -0,0 +1,10 @@
+process_event() {
+    if [[ ${1:0:9} == "workspace" ]]; then
+        active_workspace=$(awk -F '>>' '{ print $2 }' <<< ${1})
+        echo ${active_workspace}
+    fi
+}
+socat -u UNIX-CONNECT:/tmp/hypr/"$HYPRLAND_INSTANCE_SIGNATURE"/.socket2.sock - | while read -r event; do 
+    # echo "${event}"
+    process_event "${event}"
+done
