601be91abcb57bfd5ccc14b20df7dfe05ea4dc8c
bazuin-32 <ameenpiano@gmail.com>
2022-09-26 11:06:40 -0600

    Add basic implementation of dynamic workspaces for eww

--- eww/scripts/active_workspace.sh
+++ eww/scripts/active_workspace.sh
@@ -1,7 +1,10 @@
 process_event() {
     if [[ ${1:0:9} == "workspace" ]]; then
         active_workspace=$(awk -F '>>' '{ print $2 }' <<< ${1})
-        echo ${active_workspace}
+
+        workspaces_json=$(hyprctl workspaces -j | jq -rc "[ .[] | select( .id != -99 ) ] | map( . + {active: (.id == ${active_workspace})} )")
+
+        echo "${workspaces_json}"
     fi
 }
 socat -u UNIX-CONNECT:/tmp/hypr/"$HYPRLAND_INSTANCE_SIGNATURE"/.socket2.sock - | while read -r event; do 
