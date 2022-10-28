5a4450fb54330a740b10d4c8b6d260a8930de62f
bazuin-32 <ameenpiano@gmail.com>
2022-10-28 07:19:58 -0600

    Change `aliases` dir to `aliases.d`

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -4,6 +4,6 @@ unalias gp
 # source all the alias files in the subdir
 script_path="$(readlink -f -- "${0}")"
 script_dir="$(dirname -- "${script_path}")"
-for file in ${script_dir}/aliases/*.zsh; do
+for file in ${script_dir}/aliases.d/*.zsh; do
   . ${file}
 done
