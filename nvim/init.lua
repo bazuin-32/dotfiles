5a68bd107752782cd7ecbc451dd29c08ee7a13bd
bazuin-32 <ameenpiano@gmail.com>
2022-12-10 11:43:10 -0700

    Fix nvim config for running in vscode

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,9 @@
+if vim.g.vscode then
+  -- if running in vscode, use an empty config
+  -- since some extensions cause problems with it
+  return
+end
+
 local confdir = os.getenv("HOME") .. "/.config/nvim"
 
 package.path = confdir .. "/nvchad/?.lua;" .. confdir .. "/nvchad/lua/?/init.lua;" .. confdir .. "/nvchad/lua/?.lua;" .. package.path
