c1a1ea9dfb4aada65ec7d593c7bbedd7960d1b9a
bazuin-32 <ameenpiano@gmail.com>
2023-01-24 17:47:08 -0700

    Add hzf function for searching history with fzf

---
+++ zsh/functions.d/20-hzf.zsh
@@ -0,0 +1,14 @@
+function hzf() {
+    ESC=$(printf '\033')
+    BLUE="${ESC}[34m"
+    BOLD="${ESC}[1m"
+    NORMAL="${ESC}[0m"
+
+    selection=$(history \
+      | sed -E "s/^(\\s*)([0-9]+\\*{0,1})(\\s*)(.+)$/\1${BLUE}\2${NORMAL}\3${BOLD}\4${NORMAL}/" \
+      | fzf --ansi --tac --no-sort \
+      | sed -E "s/^(\\s*)([0-9]+\\*{0,1})(\\s*)(.+)$/\4/"
+    )
+
+    print -z "${selection}"
+}
