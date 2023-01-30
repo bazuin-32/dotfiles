cb94d920d35a75173b1502ad9a6b45cbc4442c60
bazuin-32 <ameenpiano@gmail.com>
2023-01-30 08:16:14 -0700

    Fix `hzf` to work correctly with `\`s

--- zsh/functions.d/20-hzf.zsh
+++ zsh/functions.d/20-hzf.zsh
@@ -7,7 +7,7 @@ function hzf() {
     selection=$(history \
       | sed -E "s/^(\\s*)([0-9]+\\*{0,1})(\\s*)(.+)$/\1${BLUE}\2${NORMAL}\3${BOLD}\4${NORMAL}/" \
       | fzf --ansi --tac --no-sort \
-      | sed -E "s/^(\\s*)([0-9]+\\*{0,1})(\\s*)(.+)$/\4/"
+      | sed -E "s/^(\\s*)([0-9]+\\*{0,1})(\\s*)(.+)$/\4/ ; s/\\\\/\\\\\\\\/g"
     )
 
     print -z "${selection}"
