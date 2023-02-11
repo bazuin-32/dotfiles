9a4ceb3cfd8cb19572433ab4f44a79a8566b7cc8
bazuin-32 <ameenpiano@gmail.com>
2023-02-10 17:55:26 -0700

    Apply shellcheck fixes to `git-clean` function

--- zsh/functions.d/20-git_clean.zsh
+++ zsh/functions.d/20-git_clean.zsh
@@ -4,7 +4,7 @@ function git-clean() {
 
   git fetch -p && \
     for branch in $(git for-each-ref --format '%(refname) %(upstream:track)' refs/heads | awk '$2 == "[gone]" {sub("refs/heads/", "", $1); print $1}'); do
-      branches+=(${branch})
+      branches+=("${branch}")
 
       if [[ ${#branch} -gt ${max_branch_name_len} ]]; then
         max_branch_name_len=${#branch}
@@ -17,9 +17,10 @@ function git-clean() {
   fi
 
   echo "The following local branches do not exist remotely and will be deleted:"
-  for branch in ${branches[@]}; do
-    branch_info=$(git branch -vvl --color=always ${branch})
+  for branch in "${branches[@]}"; do
+    branch_info=$(git branch -vvl --color=always "${branch}")
 
+    # shellcheck disable=SC2183,SC2004
     padding=$( printf '%*s' $(( ${#branch} - ${max_branch_name_len} )) )
 
     # make the branch name bold and add padding to it
@@ -29,28 +30,28 @@ function git-clean() {
     normal='\\033[0m'
     regex='^\s*(\S+)\s(\S+)\s(\[.+\])\s(.*)'
     replace="${bold}\\1${normal}${padding} ${blue}\\2${normal} ${light}\\4${normal}"
-    branch_info=$(sed -E "s/${regex}/${replace}/" <<< ${branch_info})
+    branch_info=$(sed -E "s/${regex}/${replace}/" <<< "${branch_info}")
 
 
     echo " - ${branch_info}"
   done
 
-  echo -n "\nAre you sure you want to continue? [y/N] "
-  read response
+  printf "\nAre you sure you want to continue? [y/N] "
+  read -r response
   echo ''
 
   if [[ "${response}" == "y" || "${response}" == "Y" ]]; then
-    for branch in ${branches[@]}; do
-      git branch -d ${branch}
+    for branch in "${branches[@]}"; do
+      git branch -d "${branch}"
 
       if [[ $? -ne 0 ]]; then
         echo -ne "\033[1;31mError deleting branch \`${branch}\`.\033[0m Do you want to try force deleting it? [y/N] "
-        read response
+        read -r response
 
         if [[ "${response}" == "y" || "${response}" == "Y" ]]; then
-          git branch -D ${branch}
+          git branch -D "${branch}"
         else
-          echo "Not deleting.\n"
+          printf "Not deleting.\n\n"
         fi
       fi
     done
