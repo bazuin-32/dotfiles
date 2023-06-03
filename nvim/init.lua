58f6be72e2f6e81ab4a97aa4823d296f0adcc049
bazuin-32 <ameenpiano@gmail.com>
2023-06-03 14:18:39 -0600

    Make neovim reset cursor on exit

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,6 +4,12 @@ if vim.g.vscode then
   return
 end
 
+-- reset cursor on exit
+vim.api.nvim_create_autocmd(
+  { "VimLeave", "VimSuspend" },
+  { pattern = "*", command = "set guicursor=a:ver20-blinkwait300-blinkon200-blinkoff150" }
+)
+
 local confdir = os.getenv("HOME") .. "/.config/nvim"
 
 package.path = confdir .. "/nvchad/?.lua;" .. confdir .. "/nvchad/lua/?/init.lua;" .. confdir .. "/nvchad/lua/?.lua;" .. package.path
