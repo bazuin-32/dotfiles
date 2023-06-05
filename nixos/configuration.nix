832f4a81598bc201c24ed1d2eca75ea3e2d4f71e
bazuin-32 <ameenpiano@gmail.com>
2023-06-05 12:55:36 -0600

    Enable automatic garbage collection in nix config

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -24,6 +24,12 @@
       trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
     };
 
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 14d";
+    };
+
     extraOptions = ''
       experimental-features = nix-command flakes
     '';
