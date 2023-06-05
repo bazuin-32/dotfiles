6583521bbd6cf0272e0db698171334d04fc776eb
bazuin-32 <ameenpiano@gmail.com>
2023-06-05 16:12:06 -0600

    Update formatting in flake.nix

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -32,10 +32,10 @@
     homeConfigurations."ameen@ameen-nixos-laptop" = home-manager.lib.homeManagerConfiguration {
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
 
-	modules = [
-          hyprland.homeManagerModules.default
-	  {wayland.windowManager.hyprland.enable = true;}
-	];
+      modules = [
+        hyprland.homeManagerModules.default
+        {wayland.windowManager.hyprland.enable = true;}
+      ];
     };
   };
 }
