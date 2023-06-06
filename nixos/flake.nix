62ab5dd1e43397cb132bc3b0e15bc8f588a5dc19
bazuin-32 <ameenpiano@gmail.com>
2023-06-06 10:42:42 -0600

    Rewrite flake to use `mkSystemConfig` function to generate configs for multiple devices

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -10,33 +10,38 @@
     hyprland.url = "github:hyprwm/Hyprland";
   };
 
-  outputs = { nixpkgs, home-manager, hyprland, ... }: {
-    nixosConfigurations.ameen-nixos-laptop = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
+  outputs = { nixpkgs, home-manager, hyprland, ... }:
+  let
+    mkSystemConfig = device: {
+      nixosConfigurations."ameen-nixos-${device}" = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
 
-      modules = [
-        ./laptop/hardware-configuration.nix
-        ./configuration.nix
+        modules = [
+          (./. + "/${device}/hardware-configuration.nix")
+          ./configuration.nix
 
-        home-manager.nixosModules.home-manager
-        {
-          home-manager.useGlobalPkgs = true;
-        }
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+          }
 
-        hyprland.nixosModules.default
-        {
-          programs.hyprland.enable = true;
-        }
-      ];
+          hyprland.nixosModules.default
+          {
+            programs.hyprland.enable = true;
+          }
+        ];
+      };
+
+      homeConfigurations."ameen@ameen-nixos-${device}" = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
+
+        modules = [
+          hyprland.homeManagerModules.default
+          {wayland.windowManager.hyprland.enable = true;}
+        ];
+      };
     };
-
-    homeConfigurations."ameen@ameen-nixos-laptop" = home-manager.lib.homeManagerConfiguration {
-      pkgs = nixpkgs.legacyPackages.x86_64-linux;
-
-      modules = [
-        hyprland.homeManagerModules.default
-        {wayland.windowManager.hyprland.enable = true;}
-      ];
-    };
+  in {
+    inherit (mkSystemConfig "laptop") nixosConfigurations homeConfigurations;
   };
 }
