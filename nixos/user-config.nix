8da4178bae6012938ac79948babd3e9ef9e0b798
bazuin-32 <ameenpiano@gmail.com>
2023-07-02 16:01:39 -0600

    `programs.zsh.enableSyntaxHighlighting` -> `programs.zsh.syntaxHighlighting.enable` in nix config

--- nixos/user-config.nix
+++ nixos/user-config.nix
@@ -54,7 +54,7 @@
       enable = true;
       enableAutosuggestions = true;
       enableCompletion = true;
-      enableSyntaxHighlighting = true;
+      syntaxHighlighting.enable = true;
       enableVteIntegration = true;
       autocd = false;
 
