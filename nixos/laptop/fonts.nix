b6e395e0bbbe700614ff4e7b353388ae372d01a3
bazuin-32 <ameenpiano@gmail.com>
2023-07-27 20:18:56 -0600

    Update laptop font size

--- nixos/laptop/fonts.nix
+++ nixos/laptop/fonts.nix
@@ -1,3 +1,3 @@
 { ... }: {
-  home-manager.users.ameen.programs.foot.settings.main.font = "MesloLGS NF:size=7";
+  home-manager.users.ameen.programs.foot.settings.main.font = "MesloLGS NF:size=10.5";
 }
