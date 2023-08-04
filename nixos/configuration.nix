4b63a0e00034839c3bca34ecd55c9abb490cce76
bazuin-32 <ameenpiano@gmail.com>
2023-08-03 21:45:10 -0600

    Change nix gc setting

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -25,7 +25,7 @@
     gc = {
       automatic = true;
       dates = "weekly";
-      options = "--delete-older-than 14d";
+      options = "--delete-older-than 7d";
     };
 
     extraOptions = ''
