59bf91017e665d443e5e726ce7332072ea092288
bazuin-32 <ameenpiano@gmail.com>
2023-11-12 16:57:42 -0700

    Use zsh for nix-shell

--- nixos/user-config.nix
+++ nixos/user-config.nix
@@ -325,6 +325,16 @@
           src = pkgs.lib.cleanSource ../zsh;
           file = "p10k.zsh";
         }
+        {
+          name = "zsh-nix-shell";
+          file = "nix-shell.plugin.zsh";
+          src = pkgs.fetchFromGitHub {
+            owner = "chisui";
+            repo = "zsh-nix-shell";
+            rev = "v0.7.0";
+            sha256 = "149zh2rm59blr2q458a5irkfh82y3dwdich60s9670kl3cl5h2m1";
+          };
+        }
       ];
 
       shellAliases = {
