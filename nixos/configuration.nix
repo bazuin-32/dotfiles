e758b458c3c33a7e120896ebc08cd200dcf68aa3
bazuin-32 <ameenpiano@gmail.com>
2023-12-22 09:06:07 -0700

    Rename `nssmdns` -> `nssmdns4`

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -72,7 +72,7 @@
   # Enable CUPS to print documents.
   services.printing.enable = true;
   services.avahi.enable = true;
-  services.avahi.nssmdns = true;
+  services.avahi.nssmdns4 = true;
   services.avahi.openFirewall = true;
 
   # Enable sound.
