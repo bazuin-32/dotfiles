9069718255a6a4949daa36e9163a03402d7db775
bazuin-32 <ameenpiano@gmail.com>
2024-07-02 15:13:08 -0600

    Setup battery menu panel

--- ags/ts/menu/widgets/battery.ts
+++ ags/ts/menu/widgets/battery.ts
@@ -1,3 +1,66 @@
-const BatteryPanel = () => Widget.Label("battery stuff")
+import { BatteryIndicator } from "ts/icons/battery"
+
+const battery = await Service.import("battery")
+
+const BatteryLevel = () => Widget.Box({
+    children: [
+        BatteryIndicator(),
+        Widget.Label({
+            label: battery.bind("percent").as(p => `${p}%`)
+        }),
+        Widget.LevelBar({
+            hexpand: true,
+            value: battery.bind("percent").as(p => p / 100)
+        })
+    ]
+})
+
+const BatteryStatsLeft = () => Widget.Box({
+    vertical: true,
+    hpack: "start",
+    children: [
+        Widget.Label({
+            label: "Time remaining",
+            hpack: "start"
+        }),
+        Widget.Label({
+            label: battery.bind("energy_rate").as(r => r < 0 ? "Current charge rate" : "Current discharge rate"),
+            hpack: "start"
+        })
+    ]
+})
+
+
+//                                                       hours:minutes
+const format_seconds = (s: number) => `${Math.floor(s / 3600)}:${(Math.round(s / 60) % 60).toString().padStart(2, "0")}`
+const BatteryStatsRight = () => Widget.Box({
+    vertical: true,
+    hpack: "end",
+    children: [
+        Widget.Label({
+            label: battery.bind("time_remaining").as((seconds) => format_seconds(seconds)),
+            hpack: "end"
+        }),
+        Widget.Label({
+            label: battery.bind("energy_rate").as(r => `${r} W`),
+            hpack: "end"
+        })
+    ]
+})
+
+const BatteryStats = () => Widget.CenterBox({
+    startWidget: BatteryStatsLeft(),
+    endWidget: BatteryStatsRight()
+})
+
+
+const BatteryPanel = () => Widget.Box({
+    vertical: true,
+    className: "battery-panel",
+    children: [
+        BatteryLevel(),
+        BatteryStats()
+    ]
+})
 
 export { BatteryPanel }
