4eea4534bedeeb9aab8d2d5266ae7a8f27211cc0
bazuin-32 <ameenpiano@gmail.com>
2024-07-03 09:43:46 -0600

    Switch back to main menu every time it is opened

--- ags/ts/bar/widgets/right/menubtn.ts
+++ ags/ts/bar/widgets/right/menubtn.ts
@@ -1,10 +1,13 @@
 import { Clock } from "./clock"
 import { StatusIcons } from "./icons"
-import { menuVisibility } from "ts/vars/menustate"
+import { menuVisibility, activeMenu } from "ts/vars/menustate"
 
 const MenuBtn = (monitor: number) => Widget.Button({
     className: "bar-button",
     onClicked: () => {
+        if (!menuVisibility[monitor].value) {
+            activeMenu.value = "main"
+        }
         menuVisibility[monitor].value = !menuVisibility[monitor].value
     },
     child: Widget.Box({
