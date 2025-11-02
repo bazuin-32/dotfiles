85ef89a6f526c197d371182941de50c711f97d05
bazuin-32 <ameenpiano@gmail.com>
2025-11-02 16:35:41 -0700

    Update DNS when connecting to VPN

--- nixos/laptop/vpn.nix
+++ nixos/laptop/vpn.nix
@@ -1,8 +1,17 @@
-{ ... }: {
+{ pkgs, ... }: {
   services.openvpn.servers.home = {
-    config = '' config /home/ameen/ameen-nixos-laptop.ovpn '';
+    config = ''
+      config /home/ameen/ameen-nixos-laptop.ovpn
+      script-security 2
+      up /etc/openvpn/update-resolv-conf
+      down /etc/openvpn/update-resolv-conf
+      down-pre
+    '';
     autoStart = false;
   };
+
+# make sure that dns gets updated when connecting to vpn
+  environment.etc.openvpn.source = "${pkgs.update-resolv-conf}/libexec/openvpn";
 
   # see https://nixos.wiki/wiki/OpenVPN
   # I have commented this out because it makes mounting the fs
