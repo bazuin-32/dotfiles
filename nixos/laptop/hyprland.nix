ec5149c334262c4b1d3f5169e74aced208d98e06
bazuin-32 <ameenpiano@gmail.com>
2026-08-16 16:51:29 -0600

    Update laptop config to work with second monitor

--- nixos/laptop/hyprland.nix
+++ nixos/laptop/hyprland.nix
@@ -7,6 +7,55 @@
         position = "0x0";
         scale = 1;
       }
+      {
+        output = "DP-3";
+	mode = "1920x1080@60";
+	position = "-1920x0";
+	scale = 1;
+      }
+    ];
+
+    workspace_rule = [
+      {
+        workspace = "1";
+        monitor = "DP-3";
+      }
+      {
+        workspace = "2";
+        monitor = "DP-3";
+      }
+      {
+        workspace = "3";
+        monitor = "DP-3";
+      }
+      {
+        workspace = "4";
+        monitor = "DP-3";
+      }
+      {
+        workspace = "5";
+        monitor = "DP-3";
+      }
+      {
+        workspace = "6";
+        monitor = "eDP-1";
+      }
+      {
+        workspace = "7";
+        monitor = "eDP-1";
+      }
+      {
+        workspace = "8";
+        monitor = "eDP-1";
+      }
+      {
+        workspace = "9";
+        monitor = "eDP-1";
+      }
+      {
+        workspace = "10";
+        monitor = "eDP-1";
+      }
     ];
   };
 }
